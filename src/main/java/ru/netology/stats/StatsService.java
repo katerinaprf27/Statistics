21a4f887f54c58b7d6560492cbd18b4e4c0b126a
Ekaterina Parfenteva <katerinaprf27@gmail.com>
2021-03-18 18:11:44 +0300

    Added files

---
+++ src/main/java/ru/netology/stats/StatsService.java
@@ -0,0 +1,69 @@
+package ru.netology.stats;
+
+public class StatsService {
+
+    public long totalOfAllSales(long[] salesByMonths) {
+        long sum = 0;
+        for (long sale : salesByMonths) {
+            sum += sale;
+        }
+        return sum;
+    }
+
+    public long averageSalesAmountPerMonth(long[] salesByMonths) {
+        long averageSalesAmountPerMonth = totalOfAllSales(salesByMonths) / salesByMonths.length;
+        return averageSalesAmountPerMonth;
+    }
+
+    public long numberOfMonthSalesMax(long[] salesByMonths) {
+            long currentMax = salesByMonths[0];
+            long numberOfMonthMaxSales = 0;
+            for (int i = 0; i < salesByMonths.length; i++) {
+                if (salesByMonths [i] >= currentMax) {
+                    currentMax = salesByMonths[i];
+                    numberOfMonthMaxSales = i + 1;
+                }
+            }
+            return numberOfMonthMaxSales;
+        }
+
+    public long numberOfMonthSalesMin(long[] salesByMonths) {
+        long currentMin = salesByMonths[0];
+        long numberOfMonthMinSales = 0;
+        for (int i = 0; i < salesByMonths.length; i++) {
+            if (salesByMonths [i] <= currentMin) {
+                currentMin = salesByMonths[i];
+                numberOfMonthMinSales = i + 1;
+            }
+        }
+        return numberOfMonthMinSales;
+    }
+
+    public int MonthsAmountWithSalesLowerAverage(long[] salesByMonths) {
+        int monthsAmount = 0;
+        long sum =0;
+        for (long purchase : salesByMonths) {
+            sum += purchase;
+        }
+        for (long purchase : salesByMonths) {
+            if (purchase < averageSalesAmountPerMonth(salesByMonths)) {
+                monthsAmount += 1;
+            }
+        }
+        return monthsAmount;
+
+    }
+    public int MonthsAmountWithSalesHigherAverage(long[] salesByMonths) {
+        int monthsAmount = 0;
+        long sum =0;
+        for (long purchase : salesByMonths) {
+            sum += purchase;
+        }
+        for (long purchase : salesByMonths) {
+            if (purchase > averageSalesAmountPerMonth(salesByMonths)) {
+                monthsAmount += 1;
+            }
+        }
+        return monthsAmount;
+    }
+}
---
+++ src/test/java/ru/netology/stats/StatsServiceTest.java
@@ -0,0 +1,59 @@
+package ru.netology.stats;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class StatsServiceTest {
+
+    long[] salesByMonths = {8, 15, 13, 15, 17, 20, 19, 20, 7, 14, 14, 18};
+
+    @org.junit.jupiter.api.Test
+    void totalOfAllSales() {
+        StatsService service = new StatsService();
+        long expected = 180;
+        long actual = service.totalOfAllSales(salesByMonths);
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    void averageSalesAmountPerMonth() {
+        StatsService service = new StatsService();
+        long expected = 15;
+        long actual = service.averageSalesAmountPerMonth(salesByMonths);
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    void numberOfMonthSalesMax() {
+        StatsService service = new StatsService();
+        long expected = 8;
+        long actual = service.numberOfMonthSalesMax(salesByMonths);
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    void numberOfMonthSalesMin() {
+        StatsService service = new StatsService();
+        long expected = 9;
+        long actual = service.numberOfMonthSalesMin(salesByMonths);
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    void MonthsAmountWithSalesLowerAverage() {
+        StatsService service = new StatsService();
+        long expected = 5;
+        long actual = service.MonthsAmountWithSalesLowerAverage(salesByMonths);
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    void MonthsAmountWithSalesHigherAverage() {
+        StatsService service = new StatsService();
+        long expected = 5;
+        long actual = service.MonthsAmountWithSalesHigherAverage(salesByMonths);
+        assertEquals(expected, actual);
+    }
+}
+
